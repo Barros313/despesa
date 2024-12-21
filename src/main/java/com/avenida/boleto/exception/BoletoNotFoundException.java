870348771fca7e1996d20d774e62214efb1fa2ed
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-12-21 10:57:49 -0300

    Added not found exception
    Signed-off-by: Gabriel Barros de Almeida <gbarros1337@gmail.com>

--- src/main/java/com/avenida/boleto/exception/BoletoNotFoundException.java
+++ src/main/java/com/avenida/boleto/exception/BoletoNotFoundException.java
@@ -1,4 +1,11 @@
 package com.avenida.boleto.exception;
 
-public class BoletoNotFound {
+public class BoletoNotFoundException extends RuntimeException {
+    public BoletoNotFoundException(String message) {
+        super(message);
+    }
+
+    public BoletoNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
